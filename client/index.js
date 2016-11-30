925ff3f258bdb7ca2487d8a8c013a84fba4f4caa
Amanda Fillip <afillip@windstream.net>
2016-11-30 10:02:16 -0600

    React template and server set up

---
+++ client/index.js
@@ -0,0 +1,13 @@
+// React package that deals with DOM interactions
+import ReactDOM from 'react-dom';
+
+// React package for constructing components (and all non-DOM related actions)
+import React from 'react';
+
+
+import App from './components/app';
+
+// Render that component to the DOM!
+ReactDOM.render(<App/>, document.getElementById('app'))
+
+
